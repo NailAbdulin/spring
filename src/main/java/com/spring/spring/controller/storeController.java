e6e075ae3d8d1daed5fa324a4481bc7e109e4dc1
1310098 <nail.abdulin.1993@mail.ru>
2022-10-31 17:13:46 +0300

    готовое дз

--- src/main/java/com/spring/spring/controller/storeController.java
+++ src/main/java/com/spring/spring/controller/storeController.java
@@ -1,5 +1,7 @@
 package com.spring.spring.controller;
 
+import com.spring.spring.model.Item;
+import com.spring.spring.service.StoreService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +14,20 @@ import java.util.List;
 @RequestMapping("/order")
 public class storeController {
 
+    private final StoreService storeService;
+
+    public storeController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
     @GetMapping("/add")
-    public void add(@RequestParam("id")List<Integer> ids) {
+    public void add(@RequestParam("id") List<Integer> ids) {
+        storeService.add(ids);
 
     }
+
     @GetMapping("/get")
-    public Collection<item> get() {
+    public Collection<Item> get() {
+        return storeService.get();
+    }
 }
